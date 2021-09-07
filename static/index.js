843362a059473f4272bff7e3c530a14e6bdbbf10
Prajwal S N <prajwalnadig21@gmail.com>
2021-09-07 18:30:37 +0530

    chore: fixed broken links

--- static/index.js
+++ static/index.js
@@ -1,18 +1,20 @@
 window.onload = () => {
-	localStorage.open = Date.now()
-	window.addEventListener('storage', (e) => {
-		if (e.key == "open") {
-			localStorage.duplicate = Date.now()
-		}
-		if (e.key == "duplicate")
-		{
-			alert("This page is already open in another tab/window")
-			window.close()
-		}
-	})
+  localStorage.open = Date.now()
+  window.addEventListener('storage', (e) => {
+    if (e.key == "open") {
+      localStorage.duplicate = Date.now()
+    }
+    if (e.key == "duplicate")
+    {
+      alert("This page is already open in another tab/window")
+      window.close()
+    }
+  })
 
-  // obtaining username
   let userName = prompt("Enter your username");
+  while (userName == "") {
+    userName = prompt("Username cannot be empty! Please enter a valid username")
+  }
   let msg = document.getElementById("msg");
   let log = document.getElementById("log");
   let conn = new WebSocket("ws://" + document.location.host + "/ws");
@@ -20,9 +22,8 @@ window.onload = () => {
     let item = document.createElement("div");
     item.innerHTML = "<b>Connection closed.</b>";
     log.appendChild(item);
+    item.classList.add("msgBubble");
   };
-
-
 
   conn.onmessage = (e) => {
     let msgs = e.data.split("\n");
@@ -30,15 +31,14 @@ window.onload = () => {
       let data = JSON.parse(msgs[i])
       console.log(data)
       let item = document.createElement("div");
-      // item.innerHTML = `<b>${data.userName}</b>: ${data.msg}`
       if (data.userName === userName) {
-				item.innerHTML = `<b><font color='lightgreen'>${data.userName}</font></b>: ${data.msg}`
-			} else {
-				item.innerHTML = `<b><font color='red'>${data.userName}</font></b>: ${data.msg}`
-			}
+        item.innerHTML = `<b><font color='lightgreen'>${data.userName}</font></b>: ${data.msg}`
+      } else {
+        item.innerHTML = `<b><font color='red'>${data.userName}</font></b>: ${data.msg}`
+      }
       log.appendChild(item);
       item.classList.add("msgBubble");
-	    log.scrollTop = log.scrollHeight;
+      log.scrollTop = log.scrollHeight;
     }
   };
   document.getElementById("form").onsubmit = () => {
